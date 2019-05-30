8c580e795120c5b80e9741850ab47f2eedbc7654
Nicolo <nicolo.genesio@iit.it>
2019-05-30 14:16:53 +0200

    Remove YARP as dependency from travis

--- .ci/install_debian.sh
+++ .ci/install_debian.sh
@@ -10,4 +10,4 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y gcc git clang valgrind ccache
 
 # Core dependencies
-apt-get install -y cmake libace-dev libboost-all-dev qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev libeigen3-dev
+apt-get install -y cmake libboost-all-dev qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev
--- .ci/script.sh
+++ .ci/script.sh
@@ -3,17 +3,6 @@ set -e
 
 # Install source deps
 
-# Build  YARP
-git clone https://github.com/robotology/yarp
-cd yarp
-git checkout ${YARP_BRANCH}
-mkdir build
-cd build
-cmake -G"${TRAVIS_CMAKE_GENERATOR}" -DCREATE_GUIS:BOOL=OFF -DCREATE_LIB_MATH:BOOL=ON -DCMAKE_BUILD_TYPE=${TRAVIS_BUILD_TYPE} ..
-cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
-cd ../..
-
-
 # Build, blocktest
 cd $TRAVIS_BUILD_DIR
 mkdir build && cd build
