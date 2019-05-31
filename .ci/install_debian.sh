5bd6588528b804c691c0a73ca39729505d8b191a
Nicolo <nicolo.genesio@iit.it>
2019-05-31 10:22:28 +0200

    Fix travis part 2

--- .ci/install_debian.sh
+++ .ci/install_debian.sh
@@ -11,3 +11,7 @@ apt-get install -y gcc git clang valgrind ccache
 
 # Core dependencies
 apt-get install -y cmake libboost-all-dev libpugixml-dev qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev
+
+# workaround issue in pugixml
+ln -s /usr/lib/x86_64-linux-gnu/libpugixml.so /lib/x86_64-linux-gnu/libpugixml.so.1.8
+ln -s /usr/lib/x86_64-linux-gnu/libpugixml.a /lib/x86_64-linux-gnu/libpugixml.a
