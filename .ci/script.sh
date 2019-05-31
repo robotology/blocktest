eab21d9517c01cf06124a8212347834090134123
Nicolo <nicolo.genesio@iit.it>
2019-05-31 09:54:47 +0200

    Fix travis build

--- .ci/script.sh
+++ .ci/script.sh
@@ -6,6 +6,6 @@ set -e
 # Build, blocktest
 cd $TRAVIS_BUILD_DIR
 mkdir build && cd build
-cmake ..
+cmake -Dpugixml_DIR=/usr/share/libpugixml-dev/cmake/ ..
 cmake --build . --config ${TRAVIS_BUILD_TYPE}
 cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
