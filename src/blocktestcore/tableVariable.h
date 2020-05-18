5a3b8ea8e2dea273f2a95dd771e8421bcd52029e
Luca <luca.tricerri@iit.it>
2020-05-18 16:53:32 +0200

    Added table variable

---
+++ src/blocktestcore/tableVariable.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2019 Fondazione Istituto Italiano di Tecnologia (IIT)        *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ ******************************************************************************/
+
+/**
+ * @file TableVariable.h
+ * @authors: Luca Tricerri <luca.tricerri@iit.it>
+ */
+
+#pragma once
+
+#include "api.h"
+#include "general.h"
+#include "table.h"
+
+namespace BlockTestCore
+{
+
+class TableVariable: public Table
+{ 
+    public:
+        bool Init(std::vector<std::string> tableValue) override;
+
+        std::string get() override;
+        std::string fetch() override;
+        std::string get(unsigned int position) const override;
+};
+}
