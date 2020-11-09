0056c4979b55a350a79358fd28808e3f3bf5922c
Luca <luca.tricerri@iit.it>
2020-11-09 12:40:16 +0100

    fix

--- src/blocktestcore/test.cpp
+++ src/blocktestcore/test.cpp
@@ -18,188 +18,184 @@
 #include "loggerRegister.h"
 #include "testsDepot.h"
 
-
 namespace BlockTestCore
 {
 
-Test::Test(const pugi::xml_node& nodeTest,const TestsDepot_sptr& testDepot): testDepot_(testDepot), nodeTest_(nodeTest)
-{
-
-}
-
-bool Test::load(const std::string& path)
-{
-    file_ = nodeTest_.attribute("file").value();
-    code_ = nodeTest_.attribute("code").value();
-    parallel_ = nodeTest_.attribute("parallel").as_bool();
-    repetitions_ =nodeTest_.attribute("repetitions").as_int();
-    repetitionsForTime_ =nodeTest_.attribute("repetitionsfortime").as_double();
-
-    if(repetitionsForTime_)
+    Test::Test(const pugi::xml_node &nodeTest, const TestsDepot_sptr &testDepot) : testDepot_(testDepot), nodeTest_(nodeTest)
     {
-        repetitions_=100000;//Set to maximum
     }
-        
-    if(!repetitions_)
-        return false;
 
-    pugi::xpath_node_set nodeCommandsList;
+    bool Test::load(const std::string &path)
+    {
+        file_ = nodeTest_.attribute("file").value();
+        code_ = nodeTest_.attribute("code").value();
+        parallel_ = nodeTest_.attribute("parallel").as_bool();
+        repetitions_ = nodeTest_.attribute("repetitions").as_int();
+        repetitionsForTime_ = nodeTest_.attribute("repetitionsfortime").as_double();
 
-    pugi::xml_document doc;
-    file_=calcolateTestName(file_,path);
-    if(!file_.empty())
-    {    
-        pugi::xml_parse_result result=doc.load_file(file_.c_str());
-        if(!result)
+        if (repetitionsForTime_)
         {
-            TXLOG(Severity::error)<<"Test file not found:"<<file_<<std::endl;
-            return false;
+            repetitions_ = 100000; //Set to maximum
         }
-        pugi::xpath_node_set fileNodes = doc.select_nodes(syntax::command);
-        nodeCommandsList=fileNodes;
-        TXLOG(Severity::debug)<<"file code:"<<code_<<" command num:"<<nodeCommandsList.size()<<std::endl;
-    }
-    else
-    {
-        nodeCommandsList = nodeTest_.select_nodes("command");
-        TXLOG(Severity::debug)<<"online"<<nodeCommandsList.size()<<std::endl;
-    }
-    
-    for (pugi::xpath_node_set::const_iterator it = nodeCommandsList.begin(); it != nodeCommandsList.end(); ++it)
-    {
-        auto nodeCommand = *it;
-        Command_sptr command=std::make_shared<Command>(nodeCommand.node(),shared_from_this());
-        command->load();
-        data_.emplace_back(command);
-        //TXLOG(Severity::debug)<<"file code:"<<code_<<":"<<outCommand.dumpCommand()<<std::endl;
-    }
 
-    pugi::xpath_node loggingNode = doc.select_node(syntax::logging);
-    if(loggingNode.node()!=nullptr)
-    {
-        loggingActive_ = loggingNode.node().attribute(syntax::loggingactive).as_bool();
-        loggingJoints_ = loggingNode.node().attribute(syntax::loggingpart).value();
-        loggingwrapperName_ = loggingNode.node().attribute(syntax::loggingwrappername).value();
-        std::string loggingTypeParam = loggingNode.node().attribute(syntax::loggingtype).value();
+        if (!repetitions_)
+            return false;
 
-        std::istringstream issType(loggingTypeParam);
-        std::vector<std::string> tokensForType{std::istream_iterator<std::string>{issType},std::istream_iterator<std::string>{}};
+        pugi::xpath_node_set nodeCommandsList;
 
-        std::transform(tokensForType.begin(),tokensForType.end(),std::back_inserter(loggingType_),[](const std::string& current)->loggingType 
+        pugi::xml_document doc;
+        file_ = calcolateTestName(file_, path);
+        if (!file_.empty())
+        {
+            pugi::xml_parse_result result = doc.load_file(file_.c_str());
+            if (!result)
             {
+                TXLOG(Severity::error) << "Test file not found:" << file_ << std::endl;
+                return false;
+            }
+            pugi::xpath_node_set fileNodes = doc.select_nodes(syntax::command);
+            nodeCommandsList = fileNodes;
+            TXLOG(Severity::debug) << "file code:" << code_ << " command num:" << nodeCommandsList.size() << std::endl;
+        }
+        else
+        {
+            nodeCommandsList = nodeTest_.select_nodes("command");
+            TXLOG(Severity::debug) << "online" << nodeCommandsList.size() << std::endl;
+        }
+
+        for (pugi::xpath_node_set::const_iterator it = nodeCommandsList.begin(); it != nodeCommandsList.end(); ++it)
+        {
+            auto nodeCommand = *it;
+            Command_sptr command = std::make_shared<Command>(nodeCommand.node(), shared_from_this());
+            command->load();
+            data_.emplace_back(command);
+            //TXLOG(Severity::debug)<<"file code:"<<code_<<":"<<outCommand.dumpCommand()<<std::endl;
+        }
+
+        pugi::xpath_node loggingNode = doc.select_node(syntax::logging);
+        if (loggingNode.node() != nullptr)
+        {
+            loggingActive_ = loggingNode.node().attribute(syntax::loggingactive).as_bool();
+            loggingJoints_ = loggingNode.node().attribute(syntax::loggingpart).value();
+            loggingwrapperName_ = loggingNode.node().attribute(syntax::loggingwrappername).value();
+            std::string loggingTypeParam = loggingNode.node().attribute(syntax::loggingtype).value();
+
+            std::istringstream issType(loggingTypeParam);
+            std::vector<std::string> tokensForType{std::istream_iterator<std::string>{issType}, std::istream_iterator<std::string>{}};
+
+            std::transform(tokensForType.begin(), tokensForType.end(), std::back_inserter(loggingType_), [](const std::string &current) -> loggingType {
                 return stringToLoggingType(current);
             });
-    }
+        }
 
-    pugi::xpath_node settingsNode = doc.select_node(syntax::settings);
-    if(settingsNode.node()!=nullptr)
-    {
-        wait_ = settingsNode.node().attribute(syntax::wait).as_double();
-    }
+        pugi::xpath_node settingsNode = doc.select_node(syntax::settings);
+        if (settingsNode.node() != nullptr)
+        {
+            wait_ = settingsNode.node().attribute(syntax::wait).as_double();
+        }
 
-    if(repetitions_)
-        Report::instance().addTest(code_,{testDepot_->repetitions_,repetitions_});
+        if (repetitions_)
+            Report::instance().addTest(code_, {testDepot_->repetitions_, repetitions_});
 
-    return true;
-};
-
-bool Test::valid() const
-{
-    return data_.size();
-}
-
-bool Test::isLogActive(loggingType type) const
-{
-    auto it=std::find_if(loggingType_.begin(),loggingType_.end(),[type](loggingType current)->bool
-    {
-        return type==current;
-    });
-
-    if(it!=loggingType_.end())
         return true;
-    return false;
-}
+    };
 
-execution Test::execute(bool isRealRobot,unsigned int repetition)
-{
-    if(!repetitions_)
-        return execution::continueexecution;
-
-    if(!parallel_)
-        testDepot_->waitTermination();    
-
-    execution out;
-    testThread_=std::make_unique<std::thread>([&]()
+    bool Test::valid() const
     {
-        out=work(isRealRobot,repetition);
-    });
+        return data_.size();
+    }
 
-    if(!parallel_)
-        waitTermination(); 
-    return out;
-}
-
-execution Test::work(bool isRealRobot,unsigned int currentGlobalRepetition) const
-{
-    execution out{execution::continueexecution};
-    double start=ClockFacility::instance().nowDbl();
-    for(unsigned int currentTestRepetitions=0;currentTestRepetitions<repetitions_;++currentTestRepetitions)
+    bool Test::isLogActive(loggingType type) const
     {
-        //**logging
-        if(loggingActive_)
-            {
-            logCreationFunction call;
+        auto it = std::find_if(loggingType_.begin(), loggingType_.end(), [type](loggingType current) -> bool {
+            return type == current;
+        });
+
+        if (it != loggingType_.end())
+            return true;
+        return false;
+    }
+
+    execution Test::execute(bool isRealRobot, unsigned int repetition)
+    {
+        if (!repetitions_)
+            return execution::continueexecution;
+
+        if (!parallel_)
+            testDepot_->waitTermination();
+
+        execution out;
+        testThread_ = std::make_unique<std::thread>([&]() {
+            out = work(isRealRobot, repetition);
+        });
+
+        if (!parallel_)
+            waitTermination();
+        return out;
+    }
+
+    execution Test::work(bool isRealRobot, unsigned int currentGlobalRepetition) const
+    {
+        execution out{execution::continueexecution};
+        double start = ClockFacility::instance().nowDbl();
+        for (unsigned int currentTestRepetitions = 0; currentTestRepetitions < repetitions_; ++currentTestRepetitions)
+        {
+            //**logging
             std::shared_ptr<InfoLogger> logger;
-            auto mymap=LoggerRegister::getMap();
-            if(mymap.find(testDepot_->loggingcommand_)==mymap.end())
+            if (loggingActive_)
             {
-                TXLOG(Severity::error)<<"Unknown command for logger:"<<testDepot_->loggingcommand_<<std::endl;      
-            }
-            else
-            {
-                call=LoggerRegister::getCreatorFunction(testDepot_->loggingcommand_);
-                logger=(call)(loggingJoints_,testDepot_->loggingTime_,loggingwrapperName_,code_,currentTestRepetitions);
-                logger->start();
-            }
-        }
-        //**end logger
+                logCreationFunction call;
 
-        TXLOG(Severity::info)<<"+++++Test code:"<<code_<<" Total repetitions:"<<repetitions_<<" Actual repetition:"<<currentTestRepetitions+1<<std::endl;
-        for(const Command_sptr& current:data_)
-        {
-            TestRepetitions currentRepetition{currentGlobalRepetition,currentTestRepetitions};
-            out=current->execute(isRealRobot,currentRepetition);
-            if(out==execution::stopexecution)
-            {
-                TXLOG(Severity::error)<<"Stop execution:"<<current->dumpCommand()<<std::endl;
-                continue;
+                auto mymap = LoggerRegister::getMap();
+                if (mymap.find(testDepot_->loggingcommand_) == mymap.end())
+                {
+                    TXLOG(Severity::error) << "Unknown command for logger:" << testDepot_->loggingcommand_ << std::endl;
+                }
+                else
+                {
+                    call = LoggerRegister::getCreatorFunction(testDepot_->loggingcommand_);
+                    logger = (call)(loggingJoints_, testDepot_->loggingTime_, loggingwrapperName_, code_, currentTestRepetitions);
+                    logger->start();
+                }
             }
-        }     
-        ClockFacility::instance().wait(wait_);
-        if(repetitionsForTime_ && ClockFacility::instance().nowDbl()-start>repetitionsForTime_)
-        {
-            TXLOG(Severity::debug)<<"Exit test for repetition timeout:"<<repetitionsForTime_<<" Total repetitions:"<<repetitions_<<" Actual repetition:"<<currentTestRepetitions+1<<std::endl;
-            break;
+            //**end logger
+
+            TXLOG(Severity::info) << "+++++Test code:" << code_ << " Total repetitions:" << repetitions_ << " Actual repetition:" << currentTestRepetitions + 1 << std::endl;
+            for (const Command_sptr &current : data_)
+            {
+                TestRepetitions currentRepetition{currentGlobalRepetition, currentTestRepetitions};
+                out = current->execute(isRealRobot, currentRepetition);
+                if (out == execution::stopexecution)
+                {
+                    TXLOG(Severity::error) << "Stop execution:" << current->dumpCommand() << std::endl;
+                    continue;
+                }
+            }
+            ClockFacility::instance().wait(wait_);
+            if (repetitionsForTime_ && ClockFacility::instance().nowDbl() - start > repetitionsForTime_)
+            {
+                TXLOG(Severity::debug) << "Exit test for repetition timeout:" << repetitionsForTime_ << " Total repetitions:" << repetitions_ << " Actual repetition:" << currentTestRepetitions + 1 << std::endl;
+                break;
+            }
         }
+        return out;
     }
-    return out;
-}
 
-bool Test::waitTermination() const
-{
-    if(!testThread_)
+    bool Test::waitTermination() const
+    {
+        if (!testThread_)
+            return true;
+        if (!testThread_->joinable())
+            return true;
+        TXLOG(Severity::debug) << "Wait termination for test code:" << code_ << std::endl;
+        testThread_->join();
+        TXLOG(Severity::debug) << "Exit termination for test code:" << code_ << std::endl;
         return true;
-    if(!testThread_->joinable())
-        return true;        
-    TXLOG(Severity::debug)<<"Wait termination for test code:"<<code_<<std::endl;
-    testThread_->join();
-    TXLOG(Severity::debug)<<"Exit termination for test code:"<<code_<<std::endl;
-    return true;
-}
+    }
 
-Test::~Test()
-{
-    TXLOG(Severity::debug)<<"Test destroyed code:"<<code_<<std::endl;
-}
+    Test::~Test()
+    {
+        TXLOG(Severity::debug) << "Test destroyed code:" << code_ << std::endl;
+    }
 
-}
+} // namespace BlockTestCore
