382be0d7cf8c90af479d3b2a64dbc3bba6e9df04
triccyx <triccyx@gmail.com>
2021-10-13 12:59:44 +0200

    workspace and execution wait

--- src/blocktestrunner/blockTestRunner.cpp
+++ src/blocktestrunner/blockTestRunner.cpp
@@ -11,65 +11,66 @@
  */
 
 #include "blockTestRunner.h"
-#include "logger.h"
-#include "report.h"
-#include "libraryLoader.h"
-#include "fixture.h"
+
 #include "actionRegister.h"
 #include "clockFacility.h"
+#include "fixture.h"
+#include "libraryLoader.h"
+#include "logger.h"
+#include "report.h"
 
-BlockTest::BlockTest(const std::string &name,const std::string &path):path_(path),name_(name)
-{   
-    tests_=std::make_shared<BlockTestCore::TestsDepot>();
-    BlockTestCore::Logger::instance();
-    
-    TXLOG(Severity::info)<<"*********************"<<std::endl;
-    TXLOG(Severity::info)<<"**Start Test system**"<<std::endl;
-    TXLOG(Severity::info)<<"*********************"<<std::endl;
+BlockTest::BlockTest(const std::string &name, const std::string &path) : path_(path), name_(name)
+{
+	tests_ = std::make_shared<BlockTestCore::TestsDepot>();
+	BlockTestCore::Logger::instance();
+
+	TXLOG(Severity::info) << "*********************" << std::endl;
+	TXLOG(Severity::info) << "**Start Test system**" << std::endl;
+	TXLOG(Severity::info) << "*********************" << std::endl;
 }
 
- BlockTest::~BlockTest()
- {
-     /*
-    TXLOG(Severity::debug)<<"BlockTest destroyed"<<std::endl;
+BlockTest::~BlockTest()
+{
+	/*
+   TXLOG(Severity::debug)<<"BlockTest destroyed"<<std::endl;
 
-    TXLOG(Severity::info)<<"********************"<<std::endl;
-    TXLOG(Severity::info)<<"**Stop Test system**"<<std::endl;
-    TXLOG(Severity::info)<<"********************"<<std::endl;
-    */
- }
+   TXLOG(Severity::info)<<"********************"<<std::endl;
+   TXLOG(Severity::info)<<"**Stop Test system**"<<std::endl;
+   TXLOG(Severity::info)<<"********************"<<std::endl;
+   */
+}
 
 unsigned int BlockTest::run()
 {
-    unsigned int totErrorsOut;
-    BlockTestCore::Fixture fixture(name_,path_);
-    fixture.execute();
+	unsigned int totErrorsOut;
+	BlockTestCore::Fixture fixture(name_, path_);
+	fixture.execute();
 
-    BlockTestCore::LibraryLoader loader;
-    loader.load(name_,path_);
+	BlockTestCore::LibraryLoader loader;
+	loader.load(name_, path_);
 
-    //**Only for logging
-    BlockTestCore::ActionRegister x;
-    x.Dump();
-    //**
+	//**Only for logging
+	BlockTestCore::ActionRegister x;
+	x.Dump();
+	//**
 
-    BlockTestCore::ClockFacility::instance().load(name_,path_);
+	BlockTestCore::ClockFacility::instance().load(name_, path_);
 
-    tests_->load(name_,path_);
-    if(!tests_->valid())
-    {
-        TXLOG(Severity::critical)<<"Tests not found or empty name or all repetitions are zero:"<<name_<<" path:"<<path_<<std::endl;
-    }
+	tests_->load(name_, path_);
+	if (!tests_->valid())
+	{
+		TXLOG(Severity::critical) << "Tests not found or empty name or all repetitions are zero:" << name_ << " path:" << path_ << std::endl;
+	}
 
-    tests_->execute();
-    //tests_->waitTermination();
+	tests_->execute();
+	// tests_->waitTermination();
 
-    BlockTestCore::Report::instance().dump();
+	BlockTestCore::Report::instance().dump();
 
-    totErrorsOut=BlockTestCore::Report::instance().totalErrors_;
+	totErrorsOut = BlockTestCore::Report::instance().totalErrors_;
 
-    loader.stop();
-    fixture.stop();
- 
-    return totErrorsOut;
+	loader.stop();
+	fixture.stop();
+
+	return totErrorsOut;
 }
